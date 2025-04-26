ef2ecde820632eadd1902398b68391cd4dedf66e
victors3136 <115093754+victors3136@users.noreply.github.com>
2025-04-26 16:19:01 +0300

    Update test_sequence_tokenizer.py
    Add "DeepPhonemizer." import prefix

--- tests/test_sequence_tokenizer.py
+++ tests/test_sequence_tokenizer.py
@@ -1,6 +1,6 @@
 import unittest
 
-from dp.preprocessing.text import SequenceTokenizer
+from DeepPhonemizer.dp.preprocessing.text import SequenceTokenizer
 
 
 class TestSequenceTokenizer(unittest.TestCase):
